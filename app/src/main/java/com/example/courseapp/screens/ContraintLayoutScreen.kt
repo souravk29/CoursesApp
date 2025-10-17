17708eec21abc78c6120319702c0acf966a514cb
souravk29 <sk2661901@gmail.com>
2025-10-18 01:00:34 +0530

    added dependency

--- app/src/main/java/com/example/courseapp/screens/ContraintLayoutScreen.kt
+++ app/src/main/java/com/example/courseapp/screens/ContraintLayoutScreen.kt
@@ -8,8 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.constraintlayout.widget.ConstraintLayout
-
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun ConstraintLayoutScreen(){
@@ -21,6 +20,42 @@ fun ConstraintLayoutScreen(){
             .verticalScroll(rememberScrollState())
     ){
 
+        // creating the references for composables that will be constrained within the layout
+
+        val(gradientBackground) = createRefs()
+
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+      ----------  val(gradientBackground) = createRefs()-------------
+
+
+it creates a reference (like a unique ID or a handle) named gradientBackground that you can attach to a composable (like a Box or Image).
+Once attached, you can use this reference to define layout constraints for other composables.
+
+Here’s a simple breakdown:
+
+1. The Function: createRefs()
+This function creates one or more ConstrainedLayoutReference objects.
+
+These are not UI components. They are just empty pointers that you will later assign to your actual composables.
+
+2. The Syntax: val(gradientBackground) = ...
+This is Kotlin's destructuring declaration. It's a clean way to "unpack" values from an object or function call.
+
+The createRefs() function is designed to be destructured. You could just as easily create multiple references at once
+*/
