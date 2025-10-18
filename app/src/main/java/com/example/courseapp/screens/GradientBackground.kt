b6328a1912032b9b2619727756440925f5a5376e
souravk29 <sk2661901@gmail.com>
2025-10-19 01:42:20 +0530

    update

---
+++ app/src/main/java/com/example/courseapp/screens/GradientBackground.kt
@@ -0,0 +1,25 @@
+package com.example.courseapp.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.courseapp.R
+
+
+@Composable
+fun BackgroundGradient(modifier: Modifier){
+
+    Image(
+        painter = painterResource(id = R.drawable.bkgrd),
+        contentDescription = "Main Background",
+        contentScale = ContentScale.FillBounds,
+        modifier = modifier.alpha(0.8f)                                                                         // initially here the "constrainAs" will be shown as error becoz only
+        // components defined inside the "ConstraintLayout" can benefit from
+        // "ConstraintLayout" or "ConstrainAs", thats  why we used "modifier"
+        // instead of "Modifier" => not created new modifier but used the previosly defined one from the  ContraintLayout
+        // this is the reason why we passed "modifier: Modifier" as parameter to this function
+    )
+}
